52df40c36ee87faf070ef9eea9e9e6bcad673ec0
Zhipeng-Chang <zchang0302@gmail.com>
2019-02-12 09:24:42 -0700

    upadet ci

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -134,5 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+try:
+    import django_heroku
+    django_heroku.settings(locals())
 
-django_heroku.settings(locals())
+except ImportError:
+    pass
