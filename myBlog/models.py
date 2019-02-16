f1fd684a3d309d9b6dca7be9ac9d32ccfa09ecfd
slark <610158299@qq.com>
2019-02-15 19:11:21 -0700

    database

--- myBlog/models.py
+++ myBlog/models.py
@@ -15,8 +15,51 @@ class Post(models.Model):
     )
     post_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     post_title = models.CharField(max_length=400)
+    post_content = models.TextField()
     post_type = models.CharField(max_length=32, choices=postType)
-    author_id = models.ForeignKey(User, related_name='author', on_delete=models.PROTECT)
+    author = models.ForeignKey(User, related_name='author', on_delete=models.PROTECT)
+
+    #post_comment = models.CharField(max_length=1000)
+
 
     def __str__(self):
     	return self.post_title
+
+
+
+class User(models.Model):
+    #https://blog.csdn.net/laikaikai/article/details/80563387
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_name = models.CharField(max_length=128,unique=True)
+    password = models.CharField(max_length=256)
+    friends = models.ManyToManyField('self', blank=True, null=True,related_name='friends')
+
+
+    def checkfriend(self,username):
+        if username in self.friends.all():
+            return True
+        else:
+            return False
+
+
+    def __str__(self):
+        return self.user_name
+
+
+
+
+class Comment(models.Model):     
+    post = models.ForeignKey(Post)                      
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)                                          
+    content = models.TextField()
+       
+    class Meta:
+        db_table = 'comment'
+        verbose_name_plural = u'comment'
+        
+    def __unicode__(self):
+        return self.content
+    
+    @models.permalink
+    def get_absolute_url(self):
+        return ('post_detail', (), { 'post_pk': self.post.pk })
