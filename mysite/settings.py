06012bb2910edf2714803e6aa0a0eed6487e79a5
Zhipeng-Chang <zchang0302@gmail.com>
2019-02-26 19:36:49 -0700

    fix frontend

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'myBlog/static/'
 try:
     import django_heroku
     django_heroku.settings(locals())
