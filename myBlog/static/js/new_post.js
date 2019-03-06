0c5f21d924ce96015b217bed0f21feb3f3138bf7
Zhipeng-Chang <zchang0302@gmail.com>
2019-03-06 12:03:47 -0700

    deleting works now (only on chrome)

--- myBlog/static/js/new_post.js
+++ myBlog/static/js/new_post.js
@@ -1,42 +1,37 @@
-var form = {
-    title: "",
-    content: "",
-    contentType:"",
-    categories: "",
-    visibility: "",
-    description:"",
-    visibleTo:"",
-    unlisted:""
-
-}
-
-var encoded_img="";
-
 //https://stackoverflow.com/questions/22087076/how-to-make-a-simple-image-upload-using-javascript-html
-function previewFile(){
+var encoded_img="";
+function previewFile()
+{
     var preview = document.querySelector('img'); 
     var file= document.querySelector('input[type=file]').files[0]; 
     var reader  = new FileReader();
-    reader.onloadend = function () {
+    reader.onloadend = function () 
+    {
         preview.src = reader.result;
         encoded_img = preview.src;
     }
-    if (file) {
+    if (file) 
+    {
         reader.readAsDataURL(file); //reads the data as a URL
-    } else {
+    } 
+    else 
+    {
         preview.src = "";
     }
 }
+
 // https://www.w3schools.com/jsref/prop_style_visibility.asp
-function enableInput(){
+function enableInput()
+{
     var selectedType = document.getElementById("post-contenttype").value;
-    console.log(selectedType)
-    if (selectedType=="image/png;base64" || selectedType=="image/jpeg;base64"){
+    if (selectedType=="image/png;base64" || selectedType=="image/jpeg;base64")
+    {
         alert("Since you selected img, you will not be able to add content");
         document.getElementById("post-content").readOnly  = true;
         document.getElementById("post-image").style.visibility = "visible";
     }
-    else{
+    else
+    {
         document.getElementById("post-content").readOnly  = false;
         document.getElementById("post-image").style.visibility = "hidden";
 
@@ -44,27 +39,28 @@ function enableInput(){
 }
 
 // https://stackoverflow.com/questions/6941533/get-protocol-domain-and-port-from-url
-function get_host(){
-    var url = window.location.href
+function get_host()
+{
+    var url = window.location.href;
     var arr = url.split("/");
-    var result = arr[0] + "//" + arr[2]
+    var result = arr[0] + "//" + arr[2];
     return result
 }
 
-// Reference: https://stackoverflow.com/questions/9618504/how-to-get-the-selected-radio-button-s-value
-var radios = document.getElementsByName('unlisted');
-for (var i = 0, length = radios.length; i < length; i++){
-    if (radios[i].checked){
-        if (radios[i].value == "Yes"){
-            form.unlisted = true;
-        }
-        else{
-            form.unlisted = false;
-        }
-        break;
+function post() 
+{
+    let form = 
+    {
+        title: "",
+        content: "",
+        contentType:"",
+        categories: "",
+        visibility: "",
+        description:"",
+        visibleTo:"",
+        unlisted:""
     }
-}
-function post() {
+
     form.title = document.getElementById("post-title").value;
     form.contentType = document.getElementById("post-contenttype").value;
     form.categories = document.getElementById("post-categories").value;
@@ -73,29 +69,32 @@ function post() {
     form.unlisted = document.getElementById("post-content").value;
     form.visibleTo = document.getElementById("post-visibleto").value;
     form.description = document.getElementById("post-description").value;
-    if (form.contentType == "image/png;base64" || form.contentType =="image/jepg;base64") {
+    if (form.contentType == "image/png;base64" || form.contentType =="image/jepg;base64") 
+    {
         form.content = encoded_img;
     }
 
 // Reference: https://stackoverflow.com/questions/9618504/how-to-get-the-selected-radio-button-s-value
-var radios = document.getElementsByName('unlisted');
-for (var i = 0, length = radios.length; i < length; i++){
-    if (radios[i].checked){
-        if (radios[i].value == "Yes"){
-            form.unlisted = true;
+    var radios = document.getElementsByName('unlisted');
+    for (var i = 0, length = radios.length; i < length; i++)
+    {
+        if (radios[i].checked)
+        {
+            if (radios[i].value == "Yes")
+            {
+                form.unlisted = true;
+            }
+            else
+            {
+                form.unlisted = false;
+            }
+            break;
         }
-        else{
-            form.unlisted = false;
-        }
-        break;
     }
-}
     var xhr = new XMLHttpRequest();
-    console.log("Posting")
     xhr.open("POST",  get_host()+"/myBlog/posts/", true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.setRequestHeader("X-CSRFToken", csrf_token);
     xhr.send(JSON.stringify(form));
-    console.log(JSON.stringify(form))
     window.location.replace(get_host()+"/myBlog/all/");
   }
--- myBlog/static/js/posts_list_helper.js
+++ myBlog/static/js/posts_list_helper.js
@@ -1,68 +1,71 @@
 // https://stackoverflow.com/questions/6941533/get-protocol-domain-and-port-from-url
-function get_host(){
-    var url = window.location.href
+function get_host()
+{
+    var url = window.location.href;
     var arr = url.split("/");
-    var result = arr[0] + "//" + arr[2]
+    var result = arr[0] + "//" + arr[2];
     return result
 }
 
 function getAllPosts(url) {
-    // Default options are marked with *
       return fetch(url, {
-          method: "GET", // *GET, POST, PUT, DELETE, etc.
-          mode: "cors", // no-cors, cors, *same-origin
-          cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-          credentials: "same-origin", // include, *same-origin, omit
+          method: "GET", 
+          mode: "cors", 
+          cache: "no-cache", 
+          credentials: "same-origin", 
           headers: {
-              "Content-Type": "application/json",
-              "x-csrf-token": csrf_token
-              // "Content-Type": "application/x-www-form-urlencoded",
+              "Content-Type": "application/json"
           },
-          redirect: "follow", // manual, *follow, error
-          referrer: "no-referrer", // no-referrer, *client
-        //   body: JSON.stringify(data), // body data type must match "Content-Type" header
+          redirect: "follow", 
+          referrer: "no-referrer", 
       })
       .then(response => response.json()); // parses response to JSON
-  }
+}
 
 function deletePost(id){
     let url = "/myBlog/posts/"+id;
     return fetch(url, {
-            method: "DELETE", // *GET, POST, PUT, DELETE, etc.
-            headers: {
-                "x-csrf-token": csrf_token
-            }
+        method: "DELETE", 
+        mode: "cors", 
+        cache: "no-cache", 
+        credentials: "same-origin", 
+        headers: {
+            "x-csrftoken": csrf_token
+        },
+        redirect: "follow", 
+        referrer: "no-referrer", 
     })
-    .then(response => response.json()); // parses response to JSON
+    .then(document.location.reload(true));
 }
 
+function commentPost(post, user){
 
-var form = {
-    title: "",
-    content: "",
-    contentType:"",
-    categories: "",
-    visibility: "",
-    description:"",
-    visibleTo:"",
-    unlisted:""
-
-}
-
-
-function postComment(id, comment){
-    let url = "/myBlog/posts/"+id+"/comments/";
+    let comment =
+    {
+        "query": "addComment",
+        "post":"",
+        "comment":
+        {
+            "author":"",
+            "comment":"",
+            "contentType":"text/plain"
+        }
+    }
+    comment.post=post.origin;
+    comment.comment.author= user;
+    comment.comment.comment= comment_content;
+    
+    let url = "/myBlog/posts/"+id+"/comments";
     return fetch(url, {
-            method: "POST", // *GET, POST, PUT, DELETE, etc.
-            body: JSON.stringify(comment),
-            headers: {
-                "Content-Type": "application/json",
-                "x-csrf-token": csrf_token
-            }
+        method: "POST", 
+        mode: "cors", 
+        cache: "no-cache", 
+        credentials: "same-origin", 
+        headers: {
+            "x-csrftoken": csrf_token
+        },
+        redirect: "follow", 
+        referrer: "no-referrer", 
     })
-    .then(response => response.json()); // parses response to JSON
-}
-
-function getInput(){
-    return document.getElementById("commentInput").value;
+    .then(document.location.reload(true));
 }
