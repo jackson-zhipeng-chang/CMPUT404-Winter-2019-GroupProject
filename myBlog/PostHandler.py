ae5a51ba81fa02bdb96fe8cbcb3b2dce60864483
Zhipeng-Chang <zchang0302@gmail.com>
2019-03-13 11:38:14 -0600

    bug fix....

--- myBlog/PostHandler.py
+++ myBlog/PostHandler.py
@@ -113,7 +113,7 @@ class PostToUserHandlerView(APIView):
             if (Post.objects.filter(Q(unlisted=False), Q(author_id=current_user_uuid)).exists()):
                 my_posts_list=get_list_or_404(Post.objects.order_by('-published'), Q(unlisted=False), Q(author_id=current_user_uuid))
 
-            if (Post.objects.filter(Q(unlisted=False), ~Q(author_id=current_user_uuid), Q(visibility='PRIVATE')).exists()):
+            if (Post.objects.filter(Q(unlisted=False), ~Q(author_id=current_user_uuid), Q(visibility='PUBLIC')).exists()):
                 public_posts_list = get_list_or_404(Post.objects.order_by('-published'), ~Q(author_id=current_user_uuid), Q(unlisted=False), Q(visibility='PUBLIC'))
 
             friends_list = Helpers.get_friends(current_user_uuid)
