619ac19bc09940ee684ae70c9eabe4b80b6992a8
Zhipeng-Chang <zchang0302@gmail.com>
2019-03-23 12:25:07 -0600

    make the uuid to the correct type

--- myBlog/PostHandler.py
+++ myBlog/PostHandler.py
@@ -163,7 +163,7 @@ class PostToUserHandlerView(APIView):
         isRemote = Helpers.check_remote_request(request)
         if isRemote:
             print(request.query_params['author_uuid'])
-            current_user_uuid = request.query_params['author_uuid']
+            current_user_uuid = UUID(request.query_params['author_uuid'])
         else:
             current_user_uuid = Helpers.get_current_user_uuid(request)
 
@@ -175,6 +175,7 @@ class PostToUserHandlerView(APIView):
             serveronly_posts_list=[]
             foaf_posts_list=[]
             remotePosts = []
+
             if (Post.objects.filter(Q(unlisted=False), Q(author_id=current_user_uuid)).exists()):
                 my_posts_list = get_list_or_404(Post.objects.order_by('-published'), Q(unlisted=False), Q(author_id=current_user_uuid))
 
@@ -206,6 +207,7 @@ class PostToUserHandlerView(APIView):
                             foaf_posts_list += get_list_or_404(Post.objects.order_by('-published'), Q(unlisted=False), Q(author_id=friend_of_this_friend.id),Q(visibility='FOAF'))
             
             remotePosts = pull_remote_nodes(current_user_uuid)
+
             posts_list = my_posts_list+public_posts_list+friend_posts_list+private_posts_list+serveronly_posts_list+foaf_posts_list+remotePosts
             posts_list.sort(key=lambda x: x.published, reverse=True) # https://stackoverflow.com/questions/403421/how-to-sort-a-list-of-objects-based-on-an-attribute-of-the-objects answered Dec 31 '08 at 16:42 by Triptych
             paginator = CustomPagination()
@@ -213,7 +215,7 @@ class PostToUserHandlerView(APIView):
             serializer=PostSerializer(results, many=True)
             return paginator.get_paginated_response(serializer.data) 
         else:
-            return current_user_uuid
+            return Response("User UUID not found", status=404)
 
 
 # https://stackoverflow.com/questions/19360874/pass-url-argument-to-listview-queryset answered Oct 14 '13 at 13:11 Aamir Adnan
@@ -280,8 +282,12 @@ class MyPostHandler(APIView):
 def pull_remote_nodes(current_user_uuid):
     remotePosts = []
     for node in Node.objects.all():
-        nodeURL = node.host+"service/author/posts/?author_uuid="+str(current_user_uuid)
-        response = requests.get(nodeURL, auth=requests.auth.HTTPBasicAuth(node.remoteUsername, node.remotePassword))
+        nodeURL = node.host+"service/author/posts/"
+        params = {
+            'author_uuid': current_user_uuid,
+        }
+        response = requests.get(nodeURL, auth=requests.auth.HTTPBasicAuth(node.remoteUsername, node.remotePassword), params=params)
+        print(response)
         data = json.loads(response.content.decode('utf8').replace("'", '"'))
         for i in range (0,len(data["posts"])):
             remoteAuthorJson = data["posts"][i]["author"]
