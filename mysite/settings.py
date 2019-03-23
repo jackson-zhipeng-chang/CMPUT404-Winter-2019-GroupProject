3a9e8d0a142df5b4764b69d5704909c8b6bfeeba
Zhipeng-Chang <zchang0302@gmail.com>
2019-03-22 23:53:45 -0600

    fix 415 error when send to other server

--- mysite/settings.py
+++ mysite/settings.py
@@ -187,6 +187,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.FormParser',
-        'rest_framework.parsers.MultiPartParser'
+        'rest_framework.parsers.MultiPartParser',
+        'rest_framework.parsers.JSONParser'
      )
 }
