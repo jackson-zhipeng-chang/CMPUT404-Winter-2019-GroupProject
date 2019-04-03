bc85b20c34bd1ba54e428e7bae0390bc0e0c77c6
terrence85561 <tianyi4@ualberta.ca>
2019-04-02 21:34:25 -0600

    for debugging

--- myBlog/Helpers.py
+++ myBlog/Helpers.py
@@ -365,7 +365,7 @@ def update_this_friendship(remoteNode,remote_user_uuid,request):
         "authors":local_friend_list_of_remote_user
     }
     #Get friend list of this author
-    request_url = remote_host + "service/author/"+str(remote_user_uuid)+"/friends"
+    request_url = remote_host + "service/author/"+str(remote_user_uuid)+"/friends/"
     response = requests.post(request_url,auth=HTTPBasicAuth(remote_to_node.remoteUsername,remote_to_node.remotePassword))
     if response.status_code == 200:
         response_friendlist_set = set(response.json()["authors"])
