70e0f5d121145ac4558f20be5140f5c433b2b35d
terrence85561 <tianyi4@ualberta.ca>
2019-04-04 18:18:34 -0600

    test

--- myBlog/urls.py
+++ myBlog/urls.py
@@ -55,9 +55,9 @@ urlpatterns = [
     path('authordetails/<uuid:author_id>/',Helpers.author_details,name='authordetails'),
     path('postdetails/<uuid:post_id>/', Helpers.post_details, name='postdetails'),
     path('unfriend/<uuid:friendid>/',FriendRequestHandler.UnFriend.as_view(),name='unfriend'),
-    path('acceptFriendRequest/',FriendRequestHandler.AcceptFR.as_view(),name='acceptFriendRequest')
+    path('acceptFriendRequest/',FriendRequestHandler.AcceptFR.as_view(),name='acceptFriendRequest'),
 
-    path('author/posts',PostHandler.PostToUserHandlerView.as_view())
+    path('author/posts',PostHandler.PostToUserHandlerView.as_view()),
     path('author/<uuid:user_id>/friends', FriendQueryHandler.FriendQueryHandler.as_view())
 
 ]
