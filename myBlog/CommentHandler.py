a3407d346fe7e1b4eb8bf0e31590de9281a62f8d
Zhipeng-Chang <zchang0302@gmail.com>
2019-04-06 13:23:08 -0600

    change for dispersal again

--- myBlog/CommentHandler.py
+++ myBlog/CommentHandler.py
@@ -51,6 +51,8 @@ class CommentHandler(APIView):
                             nodeURL = node.host+"posts/"+str(post.postid)+"/comments/";
                             headers = {"Content-Type": 'application/json', "Accept": 'application/json'}
                             remote_to_node = RemoteUser.objects.get(node=node)
+                            if "dispersal" in node.host:
+                                data["comment"]["author"]["url"].replace("/author/", "/service/author/")
                             print("comments data %s"%str(data))
                             data = json.dumps(data)
                             response = requests.post(nodeURL, headers=headers,data = data,auth=HTTPBasicAuth(remote_to_node.remoteUsername, remote_to_node.remotePassword))
