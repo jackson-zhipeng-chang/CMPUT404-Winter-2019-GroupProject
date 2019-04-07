9c4591bf8f50f4f1a58a4fe1b62f574a13fe545e
terrence85561 <tianyi4@ualberta.ca>
2019-04-07 12:35:51 -0600

    print for bugs

--- myBlog/PostHandler.py
+++ myBlog/PostHandler.py
@@ -320,6 +320,7 @@ class PostToUserHandlerView(APIView):
                     friends_of_this_friend =  Helpers.get_friends(friend.id)
                     print("friends of {} is {}".format(friend,friends_of_this_friend))
                     print("friend is {},his host is {}".format(friend,friend.host))
+                    print(request.get_host())
                     if request.get_host() not in friend.host:
                         remote_friends_of_this_friend = Helpers.get_remote_friends_obj_list(friend.host, friend.id)
                         friends_of_this_friend +=remote_friends_of_this_friend
